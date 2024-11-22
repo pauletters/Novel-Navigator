c8fdeba429ddf1aa0d1dd07f09d6aec6e0a6d07d
pauletters <140007528+pauletters@users.noreply.github.com>
2024-11-21 20:39:40 -0500

    updated json files

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -4,10 +4,6 @@ dotenv.config();
 
 const db = async (): Promise<typeof mongoose.connection> => {
     try {
-      if (!process.env.MONGODB_URI) {
-        throw new Error('MongoDB URI is not defined in environment variables');
-      }
-      
       const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/novelnavigator');
       console.log(`MongoDB connected: ${conn.connection.host}`);
       return conn.connection;
